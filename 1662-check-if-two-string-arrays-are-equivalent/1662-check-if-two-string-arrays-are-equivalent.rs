c1f40a0e90e5a580fe78baf2f7a4d89ef4ec2934
Lakshya Bhati <lakshyabhati24@gmail.com>
2023-12-01 11:14:44 +0530

    Time: 1 ms (89.29%), Space: 2.2 MB (14.29%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.rs
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.rs
@@ -1,13 +1,5 @@
 impl Solution {
     pub fn array_strings_are_equal(word1: Vec<String>, word2: Vec<String>) -> bool {
-        let mut s : String = String::new();
-        let mut y: String = String::new();
-        for x in word1{
-            s.push_str(&x);
-        }
-        for x in word2{
-            y.push_str(&x);
-        }
-        s==y
+        word1.concat() == word2.concat()
     }
 }
