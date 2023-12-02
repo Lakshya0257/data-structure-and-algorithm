dd467cefdedab6354a4619f66181f5f974d03129
Lakshya Bhati <lakshyabhati24@gmail.com>
2023-12-02 11:29:04 +0530

    Time: 5 ms (61.03%), Space: 2.5 MB (36.39%) - LeetHub

---
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.rs
@@ -0,0 +1,21 @@
+impl Solution {
+    pub fn winner_of_game(colors: String) -> bool {
+        if colors.len() < 3 {
+            return false;
+        }
+        let (mut a, mut b) = (0, 0);
+        colors.as_bytes().windows(3).for_each(|win| {
+            match win[0] {
+                b'A' if win[0] == win[1] && win[1] == win[2] => {
+                    a += 1;
+                }
+                b'B' if win[0] == win[1] && win[1] == win[2] => {
+                    b += 1;
+                }
+                _ => {}
+            };
+        });
+
+        return a > b;
+    }
+}
